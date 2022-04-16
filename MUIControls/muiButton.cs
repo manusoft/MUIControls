3e5845e4e25e37bdd8603fcc10e5f7755ee23cfc
Manojbabu <83714923+manusoft@users.noreply.github.com>
2022-04-16 15:05:09 +0400

    Add muiButton control.

---
+++ MUIControls/muiButton.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MUIControls
+{
+    [ToolboxBitmap(typeof(System.Windows.Forms.Button))]
+    [Description("Raises an event when user clicks it.")]
+    public class muiButton : Button
+    {
+        // Fields
+        private int borderSize = 0;
+        private int borderRadius = 0;
+        private Color borderColor = Color.Teal;
+
+        public muiButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(100, 25);
+            this.BackColor = Color.DeepSkyBlue;
+            this.ForeColor = Color.White;
+            this.Resize += new EventHandler(Button_Resize);
+        }
+
+        // Properties
+        [Category("MUI Controls")]
+        public int BorderSize
+        {
+            get => borderSize;
+            set { borderSize=value; this.Invalidate(); }
+        }
+
+        [Category("MUI Controls")]
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set { borderRadius=value; this.Invalidate(); }
+        }
+
+        [Category("MUI Controls")]
+        public Color BorderColor
+        {
+            get => borderColor;
+            set { borderColor=value; this.Invalidate(); }
+        }
+
+        // Overridden properties
+        public override Color BackColor { get => base.BackColor; set => base.BackColor=value; }
+        public override Color ForeColor { get => base.ForeColor; set => base.ForeColor=value; }
+
+        // Methods
+        private GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        // Overridden methods
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            Rectangle rectSurface = this.ClientRectangle;
+            Rectangle rectBorder = Rectangle.Inflate(rectSurface, -borderSize, -borderSize);
+            int smoothSize = 2;
+
+            if (borderSize > 0) smoothSize = borderSize;
+
+            if (borderRadius > 2) //Rounded button
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, smoothSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    //Button surface
+                    this.Region = new Region(pathSurface);
+                    //Draw surface border for HD result
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+                    //Button border                    
+                    if (borderSize >= 1)
+                        //Draw control border
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
+                }
+            }
+            else //Normal button
+            {
+                pevent.Graphics.SmoothingMode = SmoothingMode.None;
+                //Button surface
+                this.Region = new Region(rectSurface);
+                //Button border
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Parent_BackColorChanged);
+        }
+
+        private void Parent_BackColorChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if (borderRadius > this.Height) { borderRadius = this.Height; }
+        }
+    }
+}
--- MUITesting/frmMain.Designer.cs
+++ MUITesting/frmMain.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.muiButton1 = new MUIControls.muiButton();
+            this.muiButton2 = new MUIControls.muiButton();
+            this.muiButton3 = new MUIControls.muiButton();
             this.SuspendLayout();
+            // 
+            // muiButton1
+            // 
+            this.muiButton1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.muiButton1.BorderColor = System.Drawing.Color.Teal;
+            this.muiButton1.BorderRadius = 15;
+            this.muiButton1.BorderSize = 1;
+            this.muiButton1.FlatAppearance.BorderSize = 0;
+            this.muiButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.muiButton1.ForeColor = System.Drawing.Color.White;
+            this.muiButton1.Location = new System.Drawing.Point(13, 13);
+            this.muiButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.muiButton1.Name = "muiButton1";
+            this.muiButton1.Size = new System.Drawing.Size(150, 30);
+            this.muiButton1.TabIndex = 2;
+            this.muiButton1.Text = "muiButton1";
+            this.muiButton1.UseVisualStyleBackColor = false;
+            // 
+            // muiButton2
+            // 
+            this.muiButton2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.muiButton2.BorderColor = System.Drawing.Color.Teal;
+            this.muiButton2.BorderRadius = 2;
+            this.muiButton2.BorderSize = 1;
+            this.muiButton2.FlatAppearance.BorderSize = 0;
+            this.muiButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.muiButton2.ForeColor = System.Drawing.Color.White;
+            this.muiButton2.Location = new System.Drawing.Point(171, 13);
+            this.muiButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.muiButton2.Name = "muiButton2";
+            this.muiButton2.Size = new System.Drawing.Size(150, 30);
+            this.muiButton2.TabIndex = 3;
+            this.muiButton2.Text = "muiButton2";
+            this.muiButton2.UseVisualStyleBackColor = false;
+            // 
+            // muiButton3
+            // 
+            this.muiButton3.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.muiButton3.BorderColor = System.Drawing.Color.Teal;
+            this.muiButton3.BorderRadius = 5;
+            this.muiButton3.BorderSize = 1;
+            this.muiButton3.FlatAppearance.BorderSize = 0;
+            this.muiButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.muiButton3.ForeColor = System.Drawing.Color.White;
+            this.muiButton3.Location = new System.Drawing.Point(329, 13);
+            this.muiButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.muiButton3.Name = "muiButton3";
+            this.muiButton3.Size = new System.Drawing.Size(150, 30);
+            this.muiButton3.TabIndex = 4;
+            this.muiButton3.Text = "muiButton3";
+            this.muiButton3.UseVisualStyleBackColor = false;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 511);
+            this.Controls.Add(this.muiButton3);
+            this.Controls.Add(this.muiButton2);
+            this.Controls.Add(this.muiButton1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MUI Controls Tetsing";
@@ -43,6 +102,9 @@
         }
 
         #endregion
+        private MUIControls.muiButton muiButton1;
+        private MUIControls.muiButton muiButton2;
+        private MUIControls.muiButton muiButton3;
     }
 }
 
